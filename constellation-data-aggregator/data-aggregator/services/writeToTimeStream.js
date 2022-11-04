4d89668f902c7e6e0ec84a510f63800388582639
Jason Aricheta <jason.aricheta@gmail.com>
2022-11-05 05:46:20 +1300

    improved logging messaging in aggregator
    Co-authored-by: Steven Ni <steven.christopher.ni@gmail.com>

--- constellation-data-aggregator/data-aggregator/services/writeToTimeStream.js
+++ constellation-data-aggregator/data-aggregator/services/writeToTimeStream.js
@@ -6,34 +6,37 @@ const {
 const HOME_REGION = process.env.HOME_REGION || "us-west-2";
 
 async function writeToTimeStream(input, region) {
-	try {
-		const client = new TimestreamWriteClient({region: process.env.HOME_REGION});
-		console.log(typeof region)
-		const params = {
-			DatabaseName: 'constellation-timestream-db',
-			TableName: region,
-			Records: [
-				{
-					Dimensions: [
-						{
-							Name: 'test',
-							Value: 'test'
-						}
-					],
-					MeasureName: 'ten second average',
-					MeasureValue: JSON.stringify(input),
-					MeasureValueType: 'VARCHAR',
-					Time: `${Date.now()}`,
-					TimeUnit: 'MILLISECONDS'
-				},
-			]
-		};
-		const command = new WriteRecordsCommand(params);
-		const response = await client.send(command);
-		console.log(response);
-	} catch (err) {
-		console.error(err);
-	}
+  try {
+    const client = new TimestreamWriteClient({
+      region: HOME_REGION,
+    });
+    console.log(`Writing to timestream DB in: ${HOME_REGION}`);
+    console.log(typeof region);
+    const params = {
+      DatabaseName: "constellation-timestream-db",
+      TableName: region,
+      Records: [
+        {
+          Dimensions: [
+            {
+              Name: "test",
+              Value: "test",
+            },
+          ],
+          MeasureName: "ten second average",
+          MeasureValue: JSON.stringify(input),
+          MeasureValueType: "VARCHAR",
+          Time: `${Date.now()}`,
+          TimeUnit: "MILLISECONDS",
+        },
+      ],
+    };
+    const command = new WriteRecordsCommand(params);
+    const response = await client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = writeToTimeStream;
